048ab34615ad3cfaf51e283ee5b180cbd464252f
Kiruthiyan Theivendrarasa <kiruthiyant.23@uom.lk>
2025-11-03 18:09:46 +0530

    Added frontend project

---
+++ frontend/todo_login/src/components/InputField.jsx
@@ -0,0 +1,15 @@
+export default function InputField({ label, type, name, value, onChange }) {
+  return (
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2">{label}</label>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        required
+      />
+    </div>
+  );
+}
